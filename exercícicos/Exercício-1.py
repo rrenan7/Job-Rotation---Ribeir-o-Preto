87d94284bffdb57c05e67c0761863545642036d6
Renan Rocha <82057231+rrenan7@users.noreply.github.com>
2022-06-29 12:06:33 -0300

    Delete exercícicos directory

--- exercícicos/Exercício-1.py
+++
@@ -1,9 +0,0 @@
-INDICE = 13
-SOMA = 0
-K = 0
-
-while K < INDICE:
-  K = K + 1
-  SOMA = SOMA + K
-
-print(SOMA)
--- exercícicos/Exercício-5.py
+++
@@ -1,6 +0,0 @@
-palavra = input()
-j = -1
-
-for i in range(len(palavra)):
-  print(palavra[j], end='')
-  j = j - 1
